fa80482d92bde3245e73de135ebe80b662f4c017
jys.tian <jys@jystiandeMacBook-Pro.local>
2018-09-26 13:20:02 +0800

    url time

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "go.uber.org/zap"
+import (
+  "go.uber.org/zap"
+  "time"
+)
+
 func main(){
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 	logger.Info("failed to fetch URL",
 	  // Structured context as strongly typed Field values.
-	    zap.String("url", url),
+	  zap.String("url", "http://dockone.io/article/1712"),
 	      zap.Int("attempt", 3),
 	        zap.Duration("backoff", time.Second),
 	)
