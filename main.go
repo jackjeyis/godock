e9dd98a78cfd31d0dca59a9f6db40561f31d96e1
jys.tian <jys@jystiandeMacBook-Pro.local>
2018-09-26 13:09:46 +0800

    zap log

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/uber-go/zap"
+func main(){
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	logger.Info("failed to fetch URL",
+	  // Structured context as strongly typed Field values.
+	    zap.String("url", url),
+	      zap.Int("attempt", 3),
+	        zap.Duration("backoff", time.Second),
+	)
+}
